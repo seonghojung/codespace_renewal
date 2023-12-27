56f35b48edabc333d0db74a9890e40878bd8a3fd
CODESPACE THOR <144303807+seong-jin-choi@users.noreply.github.com>
2023-12-27 18:29:34 +0900

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import styled from "styled-components";
+
+const HomeWrap = styled.div``;
 function Home() {
-  return <div>home</div>;
+  return (
+    <HomeWrap>
+      <div></div>
+    </HomeWrap>
+  );
 }
 
 export default Home;
