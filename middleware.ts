c7b51a29f5595b60f9a5f5c4ad67a22cecabc094
CODESPACE THOR <144303807+seong-jin-choi@users.noreply.github.com>
2024-01-29 13:07:14 +0900

    미들웨어 수정

--- middleware.ts
+++ middleware.ts
@@ -10,5 +10,5 @@ export default createMiddleware({
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", "/(en|ko)/:path*"],
+  matcher: ["/", "/(en|ko)/:path*", "/((?!_next|_vercel|.*\\..*).*)"],
 };
