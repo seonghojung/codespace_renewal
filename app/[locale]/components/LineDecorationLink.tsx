6e7ca817c7b923ead95fd12d36c52c1f50b68324
CODESPACE THOR <144303807+seong-jin-choi@users.noreply.github.com>
2024-01-31 22:52:34 +0900

    텍스트 언더라인 애니메이션 (링크 추가)

---
+++ app/[locale]/components/LineDecorationLink.tsx
@@ -0,0 +1,29 @@
+import { styled } from "styled-components";
+import LineDecoration from "./LineDecoration";
+import RightArrowIcon from "../(home)/icon/RightArrowIcon";
+const LinkContainer = styled.div`
+  position: relative;
+  svg {
+    width: 15%;
+    height: 100%;
+    position: absolute;
+    margin-left: 5px;
+    opacity: 0.5;
+  }
+  &:hover {
+    svg {
+      opacity: 1;
+    }
+  }
+`;
+const LineDecorationLink = ({ children, color }: { children: React.ReactNode; color?: string }) => {
+  return (
+    <LineDecoration color={color}>
+      <LinkContainer>
+        {children} <RightArrowIcon fill={color} />
+      </LinkContainer>
+    </LineDecoration>
+  );
+};
+
+export default LineDecorationLink;
