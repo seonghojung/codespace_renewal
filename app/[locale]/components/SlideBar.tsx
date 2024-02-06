8e93d0ff4129ac5e79bb70d4a997215ba506b701
codespace-jake <jake@codespace.im>
2024-02-06 14:59:54 +0900

    슬라이드바 스크롤바있을 때 버전 적용
    todo. 스크롤바 없을때 버전도 만들어야함

--- app/[locale]/components/SlideBar.tsx
+++ app/[locale]/components/SlideBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 import Link from "next/link";
@@ -11,6 +11,7 @@ import LineDecoration from "./LineDecoration";
 
 import closeIcon from "../../../public/svgs/close_white.svg";
 import logoIcon from "../../../public/svgs/logo_white.svg";
+import { createPortal } from "react-dom";
 
 // interface
 interface SlideBarProps {
@@ -20,18 +21,29 @@ interface SlideBarProps {
 
 //styled-components
 
+const Test = styled.div`
+  /* width: 100%; */
+  /* height: 100vh; */
+  /* margin: 0; */
+  /* padding: 0; */
+  /* overflow-y: scroll; */
+  position: relative;
+  z-index: 2000;
+`;
+
 const SlideBarWrap = styled.div`
   position: fixed;
   top: 0;
   z-index: 100;
   width: 100%;
+  height: 100%;
   opacity: 0;
   visibility: hidden;
   background-color: #000;
   animation: fadeOut 0.5s cubic-bezier(0.19, 1, 0.22, 1) forwards;
   transition: all 0.5s ease-in-out 0.5s;
-
   &.open {
+    padding-right: 15px;
     visibility: visible;
     opacity: 1;
     transition: all 0.3s cubic-bezier(0.19, 1, 0.22, 1) 0s;
@@ -96,14 +108,21 @@ const SlideHeader = styled.header`
 const HeaderList = styled.div`
   display: flex;
   height: 108px;
-  justify-content: space-between;
   align-items: center;
+`;
+
+interface ICloseButton {
+  $open: boolean;
+}
+
+const CloseButton = styled.button<ICloseButton>`
+  position: relative;
+  right: ${(props) => (props.$open ? "0" : "-15px")};
 `;
 
 const SideMenu = styled.div`
   position: relative;
   z-index: 100;
-  height: 100vh;
 `;
 
 const MenuList = styled.ul`
@@ -115,6 +134,7 @@ const MenuList = styled.ul`
 // component
 const SlideBar = ({ openSlideBarHandler, open }: SlideBarProps) => {
   const path = usePathname();
+  const testRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (open) {
@@ -126,46 +146,60 @@ const SlideBar = ({ openSlideBarHandler, open }: SlideBarProps) => {
   }, [open]);
 
   return (
-    <header>
-      <SlideBarWrap className={open ? "open" : ""}>
-        <SlideHeader>
-          <HeaderList>
-            <Link href="/" onClick={() => openSlideBarHandler(false)}>
-              <Image src={logoIcon} alt="logo" />
-            </Link>
-            <button type="button" onClick={() => openSlideBarHandler(false)}>
-              <Image src={closeIcon} alt="close" />
-            </button>
-          </HeaderList>
-        </SlideHeader>
-        <SideMenu>
-          <MenuList>
-            <li>
-              <LineDecoration color={"white"}>
-                <Link href="/project" className={path.endsWith("/project") ? "" : ""} onClick={() => openSlideBarHandler(false)}>
-                  PROJECT
-                </Link>
-              </LineDecoration>
-            </li>
-            <li>
-              <LineDecoration color={"white"}>
-                <Link href="/services" className={path.endsWith("/services") ? "" : ""} onClick={() => openSlideBarHandler(false)}>
-                  SERVICES
-                </Link>
-              </LineDecoration>
-            </li>
-            <li>
-              <LineDecoration color={"white"}>
-                <Link href="/contact" className={path.endsWith("/contact") ? "" : ""} onClick={() => openSlideBarHandler(false)}>
-                  CONTACT
-                </Link>
-              </LineDecoration>
-            </li>
-          </MenuList>
-        </SideMenu>
-      </SlideBarWrap>
-    </header>
+    <Portal>
+      <Test>
+        <SlideBarWrap className={open ? "open" : ""}>
+          <SlideHeader>
+            <HeaderList ref={testRef}>
+              <Link href="/" onClick={() => openSlideBarHandler(false)} style={{ position: "relative", display: "block", width: "40px", marginRight: "auto" }}>
+                <Image src={logoIcon} alt="logo" />
+              </Link>
+              <CloseButton type="button" onClick={() => openSlideBarHandler(false)} $open={open}>
+                <Image src={closeIcon} alt="close" />
+              </CloseButton>
+            </HeaderList>
+          </SlideHeader>
+          <SideMenu>
+            <MenuList>
+              <li>
+                <LineDecoration color={"white"}>
+                  <Link href="/project" className={path.endsWith("/project") ? "" : ""} onClick={() => openSlideBarHandler(false)}>
+                    PROJECT
+                  </Link>
+                </LineDecoration>
+              </li>
+              <li>
+                <LineDecoration color={"white"}>
+                  <Link href="/services" className={path.endsWith("/services") ? "" : ""} onClick={() => openSlideBarHandler(false)}>
+                    SERVICES
+                  </Link>
+                </LineDecoration>
+              </li>
+              <li>
+                <LineDecoration color={"white"}>
+                  <Link href="/contact" className={path.endsWith("/contact") ? "" : ""} onClick={() => openSlideBarHandler(false)}>
+                    CONTACT
+                  </Link>
+                </LineDecoration>
+              </li>
+            </MenuList>
+          </SideMenu>
+        </SlideBarWrap>
+      </Test>
+    </Portal>
   );
 };
-
 export default SlideBar;
+
+const Portal = ({ children }: { children: ReactNode }) => {
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
+
+  if (typeof window === "undefined") return <></>;
+
+  return mounted ? createPortal(children, document.getElementById("portal") as HTMLElement) : <></>;
+};
