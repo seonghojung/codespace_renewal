cfdffa634a4fb0d45ea6ed39d0a799fbd991ee78
codespace-jake <jake@codespace.im>
2024-02-21 03:07:53 +0900

    스와이퍼 호버시 자동슬라이드 되는 로직 추가

--- app/[locale]/(home)/Swiper.tsx
+++ app/[locale]/(home)/Swiper.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import styled from "styled-components";
-import CarouselSwiper, { HorizontalImage, VerticalImage } from "../components/Swiper";
+import CarouselSwiper, { HorizontalImage, Slide1, Slide2, Slide3, VerticalImage } from "../components/Swiper";
 import swiper0 from "../../../public/images/home/swiper_0.png";
 import swiper1 from "../../../public/images/home/swiper_1.png";
 import swiper2 from "../../../public/images/home/swiper_2.png";
@@ -9,30 +9,6 @@ import swiper3 from "../../../public/images/home/swiper_3.png";
 import swiper4 from "../../../public/images/home/swiper_4.png";
 import swiper5 from "../../../public/images/home/swiper_5.png";
 
-const Title = styled.p`
-  max-width: 450px;
-  font-size: 28px;
-  font-weight: 600;
-  line-height: 1.36;
-  text-align: center;
-  color: #050411;
-
-  @media (min-width: 768px) {
-    max-width: none;
-    height: 48px;
-    font-size: 46px;
-    line-height: 1.04;
-    text-align: left;
-    br {
-      display: none;
-    }
-  }
-`;
-
-const TitleWrap = styled.div`
-  display: flex;
-  justify-content: center;
-`;
 const Container = styled.div`
   margin-top: 40px;
   @media (min-width: 768px) {
@@ -48,12 +24,12 @@ const Container = styled.div`
 `;
 
 const images = [
-  <HorizontalImage alt="swiper0" src={swiper0} key={0} />,
-  <VerticalImage alt="swiper1" src={swiper1} key={1} />,
-  <HorizontalImage alt="swiper2" src={swiper2} key={2} />,
-  <VerticalImage alt="swiper3" src={swiper3} key={3} />,
-  <HorizontalImage alt="swiper4" src={swiper4} key={4} />,
-  <VerticalImage alt="swiper5" src={swiper5} key={5} />,
+  <Slide1 alt="swiper0" src={swiper0} key={0} />,
+  <Slide2 alt="swiper1" src={swiper1} key={1} />,
+  <Slide3 alt="swiper2" src={swiper2} key={2} />,
+  <Slide1 alt="swiper3" src={swiper3} key={3} />,
+  <Slide2 alt="swiper4" src={swiper4} key={4} />,
+  <Slide3 alt="swiper5" src={swiper5} key={5} />,
 ];
 
 const MainSwiper = () => {
--- app/[locale]/components/Swiper.tsx
+++ app/[locale]/components/Swiper.tsx
@@ -1,18 +1,39 @@
 import styled from "styled-components";
-import { Autoplay } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 import Image from "next/image";
+import { useCallback, useEffect, useRef, useState } from "react";
 const DefaultImage = styled(Image)`
-  border-radius: 12px;
   display: block;
-  object-fit: contain;
-  margin-right: 33px;
+  object-fit: cover;
+`;
+
+export const Slide1 = styled(DefaultImage)`
+  width: 246px;
+  height: 246px;
   @media (min-width: 768px) {
-    margin-right: 60px;
+    width: 256px;
+    height: 256px;
   }
 `;
+export const Slide2 = styled(DefaultImage)`
+  width: 246px;
+  height: 197px;
+  @media (min-width: 768px) {
+    width: 256px;
+    height: 205px;
+  }
+`;
+export const Slide3 = styled(DefaultImage)`
+  width: 246px;
+  height: 307px;
+  @media (min-width: 768px) {
+    width: 256px;
+    height: 320px;
+  }
+`;
+
 export const VerticalImage = styled(DefaultImage)`
   width: 234px;
   height: 300px;
@@ -31,7 +52,6 @@ export const HorizontalImage = styled(DefaultImage)`
 `;
 
 const SwiperStyle = styled(Swiper)`
-  /* margin-top: 40px; */
   .swiper-wrapper {
     -webkit-transition-timing-function: linear !important;
     -o-transition-timing-function: linear !important;
@@ -40,17 +60,114 @@ const SwiperStyle = styled(Swiper)`
     align-items: center;
     .swiper-slide {
       width: auto;
+      margin-left: 20px;
+
+      @media (min-width: 768px) {
+        &:first-child {
+          margin-left: 40px;
+        }
+        &:not(:first-child) {
+          margin-left: 60px;
+        }
+      }
     }
   }
 `;
 
 const CarouselSwiper = ({ items }: { items: any[] }) => {
+  const swiperRef = useRef<SwiperClass | null>(null);
+  const [isPrevHovering, setIsPrevHovering] = useState(false);
+  const [isNextHovering, setIsNextHovering] = useState(false);
+
+  // Prev 버튼에 대한 이벤트 처리
+  const prevHoverHandler = () => {
+    setIsPrevHovering(true);
+  };
+
+  const unPrevhoverHandler = () => {
+    setIsPrevHovering(false);
+  };
+
+  useEffect(() => {
+    let interval: any;
+
+    if (isPrevHovering) {
+      interval = setInterval(() => {
+        if (!swiperRef.current) {
+          return;
+        }
+        swiperRef.current.slidePrev(600);
+      }, 1);
+    }
+    return () => clearInterval(interval);
+  }, [isPrevHovering]);
+
+  // Next 버튼에 대한 이벤트 처리
+
+  const nextHoverHandler = () => {
+    setIsNextHovering(true);
+  };
+
+  const unNexthoverHandler = () => {
+    setIsNextHovering(false);
+  };
+
+  useEffect(() => {
+    let interval: any;
+
+    if (isNextHovering) {
+      interval = setInterval(() => {
+        if (!swiperRef.current) {
+          return;
+        }
+        swiperRef.current.slideNext(700);
+      }, 1);
+    }
+    return () => clearInterval(interval);
+  }, [isNextHovering]);
+
   return (
-    <SwiperStyle slidesPerView={"auto"} loop modules={[Autoplay]}>
-      {items.map((item, index) => {
-        return <SwiperSlide key={index}>{item}</SwiperSlide>;
-      })}
-    </SwiperStyle>
+    <div style={{ position: "relative" }}>
+      <SwiperStyle
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
+        }}
+        spaceBetween={50}
+        slidesPerView={"auto"}
+        loop
+      >
+        {items.map((item, index) => {
+          return <SwiperSlide key={index}>{item}</SwiperSlide>;
+        })}
+      </SwiperStyle>
+      <button
+        type="button"
+        style={{
+          cursor: "url(/svgs/arrow-left.svg),auto",
+          position: "absolute",
+          top: "0",
+          zIndex: "5",
+          width: "30%",
+          height: "100%",
+        }}
+        onMouseEnter={prevHoverHandler}
+        onMouseLeave={unPrevhoverHandler}
+      ></button>
+      <button
+        type="button"
+        style={{
+          cursor: "url(/svgs/arrow-right.svg),auto",
+          position: "absolute",
+          top: "0",
+          right: "0",
+          zIndex: "5",
+          width: "30%",
+          height: "100%",
+        }}
+        onMouseEnter={nextHoverHandler}
+        onMouseLeave={unNexthoverHandler}
+      ></button>
+    </div>
   );
 };
 
