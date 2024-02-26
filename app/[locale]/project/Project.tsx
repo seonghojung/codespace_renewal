005bba661b99cc5e14a6a430176da0cca5ba8131
CODESPACE THOR <144303807+seong-jin-choi@users.noreply.github.com>
2024-02-26 09:45:11 +0900

    버그 수정 및 변경점 확인

--- app/[locale]/project/Project.tsx
+++ app/[locale]/project/Project.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import Projects from "../components/Projects";
 import { Layout } from "../components/navigation";
